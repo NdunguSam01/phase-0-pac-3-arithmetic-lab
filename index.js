8bdfec9fbb6d9b88a222efcc913a48846417e5a7
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-09-26 18:13:36 +0300

    Assignment completed but npm test has issues

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function add(x, y)
+{
+    return x += y;
+}
+function subtract(x, y)
+{
+    return x -= y;
+}
+function multiply(x, y)
+{
+    return x *= y;
+}
+function divide(x, y)
+{
+    return x /= y;
+}
 function increment(n)
 {
     return n+=1;
@@ -14,6 +30,10 @@ function preserveDecimal(string)
 {
     return parseFloat(string)
 }
+console.log(add(5,20))
+console.log(subtract(150,25))
+console.log(multiply(5,20))
+console.log(divide(1500,20))
 console.log(increment(5))
 console.log(decrement(5))
 console.log(makeInt("50000"))
